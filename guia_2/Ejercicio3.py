0c2e2717264e01af84e63a0eb1e532f9354505ec
lorenzogalaverna <lorenzo.galaverna1@gmail.com>
2022-04-22 11:16:17 -0300

    ejercicio 3

---
+++ guia_2/Ejercicio3.py
@@ -0,0 +1,30 @@
+class Triangulo:
+    def inicializar (self, lado1, lado2, lado3):
+        self.lado1 = lado1
+        self.lado2 = lado2
+        self.lado3 = lado3
+
+    def lado_mayor (self):
+        if self.lado1 > self.lado2 and self.lado1 > self.lado3:
+            print("El lado mayor es el lado 1")
+        elif self.lado2 > self.lado1 and self.lado2 > self.lado3:
+            print("El lado mayor es el lado 2")
+        elif self.lado3 > self.lado2 and self.lado3 > self.lado1:
+            print("El lado mayor es el lado 3")
+        else:
+            print("no hay un lado mayor")
+
+    def equilatero (self):
+        if self.lado1 == self.lado2 and self.lado1 == self.lado3:
+            print("el triangulo es equilatero")
+        else:
+            print("el triangulo no es equilatero")
+
+
+lado1 = input("ingrese la medida del lado 1 ")
+lado2 = input("ingrese la medida del lado 2 ")
+lado3 = input("ingrese la medida del lado 3 ")
+triangulo1 = Triangulo()
+triangulo1.inicializar(lado1, lado2, lado3)
+triangulo1.lado_mayor()
+triangulo1.equilatero()
