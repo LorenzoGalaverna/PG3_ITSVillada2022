7e3bac3ef9583a343ece08e35302121f932ded49
lorenzogalaverna <lorenzo.galaverna1@gmail.com>
2022-04-29 11:48:36 -0300

    ejercicio 2

---
+++ 3_Manejo_de_exceptiones_con_python/ejercicio2.py
@@ -0,0 +1,14 @@
+while True:
+        try:
+            numero1 = int(input("ingrese un numero: "))
+            numero2 = int(input("ingrese otro numero: "))
+            print("el resultado de la division de los numeros es: ", numero1 / numero2)
+            decision = input("desea continuar? (si/no): ")
+            if decision == "si":
+                continue
+            elif decision == "no":
+                break
+            else:
+                print("ingrese una opcion valida")
+        except ZeroDivisionError:
+            print("no se puede dividir entre 0")
