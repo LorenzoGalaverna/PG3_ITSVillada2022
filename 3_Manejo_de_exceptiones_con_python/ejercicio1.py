8b984ee55194fc1fabfec90c339c1003fff67ae3
lorenzogalaverna <lorenzo.galaverna1@gmail.com>
2022-04-29 11:44:19 -0300

    ejercicio 1 y creacion de carpeta

---
+++ 3_Manejo_de_exceptiones_con_python/ejercicio1.py
@@ -0,0 +1,14 @@
+while True:
+        try:
+            numero1 = int(input("ingrese un numero: "))
+            numero2 = int(input("ingrese otro numero: "))
+            print("el resultado de la suma de los numeros es: ", numero1 + numero2)
+            decision = input("desea continuar? (si/no): ")
+            if decision == "si":
+                continue
+            elif decision == "no":
+                break
+            else:
+                    print("ingrese una opcion valida")
+        except ValueError:
+            print("por favor esciba un numero y no letras u otrosa cosas")
